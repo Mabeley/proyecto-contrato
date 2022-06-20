abc075004189c7fad3e509f7f88b9ecae545bbba
U20171B510 - Magin Madrid <u20171b510@upc.edu.pe>
2022-06-20 03:30:46 -0500

    Subiendo Path Envío

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CargaEnvioComponent } from './carga-envio/carga-envio.component';
 
 const routes: Routes = [
   {path: '',
   redirectTo:'/principal',
  pathMatch:"full"
-  }
+  },
+  {path:'carga-envio', component:CargaEnvioComponent}
 ];
 
 @NgModule({
